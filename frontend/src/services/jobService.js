7334c5bd8664d01707873972fd3cae7598897a71
Pravanshu Samadhiya <pravanshusamadhiya685@gmail.com>
2025-05-10 16:15:10 +0530

    Backend link Updated

--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:4000/api/jobs"; 
+const API_BASE_URL = "https://joblist-9ngj.onrender.com/api/jobs"; 
 
 export const fetchJobs = async () => {
   try {
